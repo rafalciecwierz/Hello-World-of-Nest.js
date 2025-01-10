56ed5c4d30882bf60c8cfe108d3173a3239a33a5
Rafał Ciećwierz <rafal.ciecwierz4@gmail.com>
2025-01-10 15:51:02 +0100

    Add: basic CRUD and validation

--- project-05-task-management/src/main.ts
+++ project-05-task-management/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
   Logger.log('App running on port 3000');
 }
---
+++ project-05-task-management/src/tasks/tasks.service.ts
@@ -0,0 +1,72 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getAllTasks() {
+        return this.tasks;
+    }
+
+    getTasksWithFilter(filterDto: GetTaskFilterDto): Task[] {
+        const { status, search } = filterDto;
+
+        let tasks = this.getAllTasks();
+
+        if (status) {
+            tasks = tasks.filter((task) => task.status === status);
+        }
+
+        if (search) {
+            tasks = tasks.filter((task) => {
+                if (task.title.includes(search) || task.description.includes(search)) {
+                    return true;
+                }
+                return false;
+            });
+        }
+
+        return tasks;
+    }
+
+
+    getTaskById(id: string): Task {
+        const found = this.tasks.find((task) => task.id === id);
+
+        if (!found) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+
+        return found;
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        }
+
+        this.tasks.push(task);
+        return task;
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        const newTask = this.getTaskById(id);
+        newTask.status = status;
+        return newTask;
+    }
+
+    deleteTask(id: string): void {
+        const found = this.getTaskById(id);
+        if (found) {
+            this.tasks = this.tasks.filter((task) => task.id !== id);
+        }
+    }
+}
